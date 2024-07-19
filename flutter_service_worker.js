56496e7ade72517a42012ece94ba52e9a4ca5ed8
Vinayak Chandra Suryavanshi <vinayak.chandra.suryavanshi@gmail.com>
2024-07-19 18:27:58 +0530

    release 1.2.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e46b8469a066761fc63135a32cab5972",
+const RESOURCES = {"flutter_bootstrap.js": "18fa2fb639b1d89d861e79f7a7fda56b",
 "version.json": "262f70521d999688488fe8a26fa921cc",
 "index.html": "c362c2fdf7ea887d9cbffae2d3b92fd6",
 "/": "c362c2fdf7ea887d9cbffae2d3b92fd6",
-"main.dart.js": "7d3a7b645db5e2a1edd548fbe75e0037",
+"main.dart.js": "75b5a56f21f3a723034b41acd44b9f76",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "e46b8469a066761fc63135a32cab5972",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/fonts/MaterialIcons-Regular.otf": "728778c851035545a6649bafc58c81ca",
+"assets/fonts/MaterialIcons-Regular.otf": "3220feaeea65caf7f17f4359fbf65281",
 "canvaskit/skwasm.js": "5d4f9263ec93efeb022bb14a3881d240",
 "canvaskit/skwasm.js.symbols": "c3c05bd50bdf59da8626bbe446ce65a3",
 "canvaskit/canvaskit.js.symbols": "74a84c23f5ada42fe063514c587968c6",
